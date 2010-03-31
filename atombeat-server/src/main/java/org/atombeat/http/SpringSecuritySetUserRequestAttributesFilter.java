0dfc452681fd5d2b88738154f1413865e4c75a4a
alimanfoo@gmail.com <alimanfoo@gmail.com@8b78bed6-e42f-f712-7edc-ac62805ffeaa>
2010-03-31 14:16:35 +0000

    import source from atombeat project; add jars and fix build path to get compile success

---
+++ atombeat-server/src/main/java/org/atombeat/http/SpringSecuritySetUserRequestAttributesFilter.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package org.atombeat.http;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.atombeat.http.HttpFilter;
+import org.exist.xquery.value.ValueSequence;
+import org.exist.xquery.value.StringValue;
+import org.springframework.security.Authentication;
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.context.SecurityContextHolder;
+
+/**
+ * @author aliman
+ *
+ */
+public class SpringSecuritySetUserRequestAttributesFilter extends HttpFilter {
+
+	
+	
+	
+	private Log log = LogFactory.getLog(this.getClass());
+	
+	
+	
+	public static final String USERNAMEREQUESTATTRIBUTEKEY = "user-name";
+	public static final String USERROLESREQUESTATTRIBUTEKEY = "user-roles";
+	
+	
+	
+	
+	@Override
+	public void doHttpFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+		log.debug("request inbound");
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
+		if (authentication != null) {
+			
+			String name = authentication.getName();
+			log.debug("found username: "+name);
+			
+			request.setAttribute(USERNAMEREQUESTATTRIBUTEKEY, new StringValue(name));
+			
+			GrantedAuthority[] authorities = authentication.getAuthorities();
+
+			ValueSequence roles = new ValueSequence();
+			
+			for (GrantedAuthority a : authorities) {
+				log.debug("found role: "+a.toString());
+				StringValue s = new StringValue(a.toString());
+				roles.add(s);
+			}
+
+			request.setAttribute(USERROLESREQUESTATTRIBUTEKEY, roles);
+
+		}
+		
+		chain.doFilter(request, response);
+
+		log.debug("response outbound");
+	}
+
+}
