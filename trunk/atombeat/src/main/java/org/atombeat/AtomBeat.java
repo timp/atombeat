cbac431f5d8ffc02c8a6683f04401e2f2c7337ef
alimanfoo <alimanfoo@8b78bed6-e42f-f712-7edc-ac62805ffeaa>
2011-02-03 09:04:01 +0000

    remove legacy

--- trunk/atombeat/src/main/java/org/atombeat/AtomBeat.java
+++
@@ -1,8 +0,0 @@
-package org.atombeat;
-
-public class AtomBeat {
-
-	public static final String REL_SECURITY_DESCRIPTOR = "http://purl.org/atombeat/rel/security-descriptor";
-	public static final String REL_MEDIA_SECURITY_DESCRIPTOR = "http://purl.org/atombeat/rel/media-security-descriptor";
-	
-}
